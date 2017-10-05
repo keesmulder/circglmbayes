b2075597e84d68e7d8c8aca84f8af3b3be27bfd4
Kees Mulder <keestimmulder@gmail.com>
2017-10-05 11:33:18 +0200

    Add Shiny app documentation

--- R/cglmShiny.R
+++ R/cglmShiny.R
@@ -1,8 +1,19 @@
+#' cglmShiny
+#'
+#' Run a shiny app interface for this package. Provides a point-and-click interface where the user can load their own data.
+#'
 #' @export
+#'
+#' @examples
+#'
+#' \dontrun{
+#' cglmShiny()
+#' }
+#'
 cglmShiny <- function() {
   appDir <- system.file("shiny", package = "CircGLMBayes")
   if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
+    stop("Could not find shiny directory. Try re-installing `mypackage`.", call. = FALSE)
   }
 
   shiny::runApp(appDir, display.mode = "normal")
